3cb375e98d954327ef97cf010530fab66ba23138
Alan Bohannon <alanbohannon@gmail.com>
2024-07-28 15:34:40 -0500

    add hiwonder robot class

---
+++ minialoha/utils/hiwonder_robot.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from hiwonderbuslinker.bus_control import ServoBus
+from hiwonderbuslinker.lewansoul_servo_bus import ServoBusCommunication
+
+from minialoha.utils.robot import Robot
+
+
+class HiwonderRobot(Robot):
+    """Interface for controlling a robot."""
+
+    def __init__(
+        self, port="COM1", leader_id=1, pre_existing_servo_ids=[2, 3, 4]
+    ) -> None:
+        super().__init__()
+        self.servo_bus = ServoBus(port, leader_id, pre_existing_servo_ids)
+        self.servo_bus_communication = ServoBusCommunication(port)
+
+    def read_position(self) -> List[int]:
+        positions = self.servo_bus.get_bus_position(self.servo_bus_communication)
+        return [pos for pos in positions.values()]
+
+    def set_goal_position(self, position: List[int]):
+        pass
+
+    def disable_robot(self):
+        pass
