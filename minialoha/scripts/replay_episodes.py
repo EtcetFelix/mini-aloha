e47e18c583cb8a1ba300c511c4eba47ed1f960c6
Alan Bohannon <alanbohannon@gmail.com>
2024-07-31 15:01:41 -0500

    set data_dir default if none is given in command line

--- minialoha/scripts/replay_episodes.py
+++ minialoha/scripts/replay_episodes.py
@@ -3,6 +3,7 @@ import os
 
 import h5py
 import IPython
+from dotenv import load_dotenv
 
 from minialoha.scripts.real_env import make_real_env
 from minialoha.utils.robot_manager import RobotManager
@@ -37,9 +38,17 @@ def main(args):
 
 
 if __name__ == "__main__":
+    load_dotenv()
     parser = argparse.ArgumentParser()
+    # set data_dir default if none is given in command line
+    data_dir = os.environ.get("DATA_DIR")
     parser.add_argument(
-        "--dataset_dir", action="store", type=str, help="Dataset dir.", required=True
+        "--dataset_dir",
+        action="store",
+        type=str,
+        help="Dataset dir.",
+        required=False,
+        default=data_dir,
     )
     parser.add_argument(
         "--episode_idx", action="store", type=int, help="Episode index.", required=False
